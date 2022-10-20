04b91dc5286db08a7b89495343f140b527e0c072
HarrySparks <116262821+HarrySparks@users.noreply.github.com>
2022-10-20 10:46:59 +0100

    Added images and started the login system

--- KanburyCinema/Form1.Designer.cs
+++ KanburyCinema/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // 
             // buttonPrevious
             // 
-            this.buttonPrevious.Location = new System.Drawing.Point(-7, 477);
+            this.buttonPrevious.Location = new System.Drawing.Point(12, 477);
             this.buttonPrevious.Name = "buttonPrevious";
             this.buttonPrevious.Size = new System.Drawing.Size(62, 38);
             this.buttonPrevious.TabIndex = 0;
@@ -56,9 +56,9 @@
             // 
             // buttonNext
             // 
-            this.buttonNext.Location = new System.Drawing.Point(214, 465);
+            this.buttonNext.Location = new System.Drawing.Point(274, 477);
             this.buttonNext.Name = "buttonNext";
-            this.buttonNext.Size = new System.Drawing.Size(120, 50);
+            this.buttonNext.Size = new System.Drawing.Size(62, 38);
             this.buttonNext.TabIndex = 1;
             this.buttonNext.Text = "Next";
             this.buttonNext.UseVisualStyleBackColor = true;
@@ -67,7 +67,7 @@
             // labelRunningTime
             // 
             this.labelRunningTime.AutoSize = true;
-            this.labelRunningTime.Location = new System.Drawing.Point(64, 448);
+            this.labelRunningTime.Location = new System.Drawing.Point(64, 428);
             this.labelRunningTime.Name = "labelRunningTime";
             this.labelRunningTime.Size = new System.Drawing.Size(76, 13);
             this.labelRunningTime.TabIndex = 2;
@@ -75,22 +75,22 @@
             // 
             // textBoxRunningTime
             // 
-            this.textBoxRunningTime.Location = new System.Drawing.Point(177, 459);
+            this.textBoxRunningTime.Location = new System.Drawing.Point(138, 425);
             this.textBoxRunningTime.Name = "textBoxRunningTime";
             this.textBoxRunningTime.Size = new System.Drawing.Size(120, 20);
             this.textBoxRunningTime.TabIndex = 3;
             // 
             // textBoxMainStar
             // 
-            this.textBoxMainStar.Location = new System.Drawing.Point(153, 406);
+            this.textBoxMainStar.Location = new System.Drawing.Point(138, 397);
             this.textBoxMainStar.Name = "textBoxMainStar";
-            this.textBoxMainStar.Size = new System.Drawing.Size(92, 20);
+            this.textBoxMainStar.Size = new System.Drawing.Size(120, 20);
             this.textBoxMainStar.TabIndex = 5;
             // 
             // labelMainStar
             // 
             this.labelMainStar.AutoSize = true;
-            this.labelMainStar.Location = new System.Drawing.Point(45, 409);
+            this.labelMainStar.Location = new System.Drawing.Point(64, 400);
             this.labelMainStar.Name = "labelMainStar";
             this.labelMainStar.Size = new System.Drawing.Size(55, 13);
             this.labelMainStar.TabIndex = 4;
@@ -98,7 +98,7 @@
             // 
             // textBoxDirector
             // 
-            this.textBoxDirector.Location = new System.Drawing.Point(177, 371);
+            this.textBoxDirector.Location = new System.Drawing.Point(138, 371);
             this.textBoxDirector.Name = "textBoxDirector";
             this.textBoxDirector.Size = new System.Drawing.Size(120, 20);
             this.textBoxDirector.TabIndex = 7;
@@ -106,7 +106,7 @@
             // labelDirector
             // 
             this.labelDirector.AutoSize = true;
-            this.labelDirector.Location = new System.Drawing.Point(30, 374);
+            this.labelDirector.Location = new System.Drawing.Point(64, 374);
             this.labelDirector.Name = "labelDirector";
             this.labelDirector.Size = new System.Drawing.Size(47, 13);
             this.labelDirector.TabIndex = 6;
@@ -122,7 +122,7 @@
             // labelCertificate
             // 
             this.labelCertificate.AutoSize = true;
-            this.labelCertificate.Location = new System.Drawing.Point(64, 352);
+            this.labelCertificate.Location = new System.Drawing.Point(64, 348);
             this.labelCertificate.Name = "labelCertificate";
             this.labelCertificate.Size = new System.Drawing.Size(57, 13);
             this.labelCertificate.TabIndex = 8;
@@ -130,7 +130,7 @@
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(106, 300);
+            this.textBoxName.Location = new System.Drawing.Point(138, 319);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(120, 20);
             this.textBoxName.TabIndex = 11;
@@ -138,16 +138,18 @@
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(30, 300);
+            this.labelName.Location = new System.Drawing.Point(62, 322);
             this.labelName.Name = "labelName";
             this.labelName.Size = new System.Drawing.Size(70, 13);
             this.labelName.TabIndex = 10;
             this.labelName.Text = "Movie Name:";
+            this.labelName.Click += new System.EventHandler(this.labelName_Click);
             // 
             // pictureBoxMovie
             // 
             this.pictureBoxMovie.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBoxMovie.Location = new System.Drawing.Point(122, 12);
+            this.pictureBoxMovie.Image = global::KanburyCinema.Properties.Resources.Creed_II_image;
+            this.pictureBoxMovie.Location = new System.Drawing.Point(46, -4);
             this.pictureBoxMovie.Name = "pictureBoxMovie";
             this.pictureBoxMovie.Size = new System.Drawing.Size(246, 301);
             this.pictureBoxMovie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
---
+++ KanburyCinema/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+
+
+namespace KanburyCinema
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openForm1()
+        {
+            new Form1().Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openForm1();
+        }
+
+    }
+}
